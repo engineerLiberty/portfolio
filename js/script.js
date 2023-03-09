3316b492c4ecac67ff680bd04e7277e9abad541c
IMOBI LIBERTY <107262741+engineerLiberty@users.noreply.github.com>
2023-03-10 00:56:49 +0100

    my portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ====================== typing animation ==================== */
+var typed = new Typed(".typing",{
+    strings:["","Software Engineer"," Java Backend Developer"],
+    typedSpeed:0,
+    Backspeed:0,
+    // loop:true
+});
